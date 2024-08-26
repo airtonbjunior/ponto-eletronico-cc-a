438f5dada6ac97115706626a986154c697cbf455
airtonbjunior <airtonbjunior@gmail.com>
2024-08-26 10:52:02 -0300

    final aula

--- js/index.js
+++ js/index.js
@@ -1,34 +1,46 @@
 const diaSemana = document.getElementById("dia-semana");
 const dataAtual = document.getElementById("data-atual");
 const horaAtual = document.getElementById("hora-atual");
+const btnRegistrarPonto = document.getElementById("btn-registrar-ponto");
+
+btnRegistrarPonto.addEventListener("click", register);
+
+diaSemana.textContent = getWeekDay();
+dataAtual.textContent = getCurrentDate();
+
+
+function register() {
+    alert("Bater ponto!");
+}
 
 
 function updateContentHour() {
-    dataAtual.textContent = getCurrentDate();
     horaAtual.textContent = getCurrentTime();
 }
 
 // Retorna a hora atual (hora/minuto/segundo)
 function getCurrentTime() {
     const date = new Date();
-    return date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+    return String(date.getHours()).padStart(2, '0') + ":" + String(date.getMinutes()).padStart(2, '0') + ":" + String(date.getSeconds()).padStart(2, '0');
 }
 
 // Retorna a data atual no padrão dd/mm/aaaa
 function getCurrentDate() {
     const date = new Date(); 
     let mes = date.getMonth() + 1;
-    return date.getDate() + "/" + mes + "/" +  date.getFullYear();
+    return String(date.getDate()).padStart(2, '0') + "/" + String(mes).padStart(2, '0') + "/" +  String(date.getFullYear()).padStart(2, '0');
 }
 
 function getWeekDay() {
-    // date.getDay()
-    // 0 - 6
-    // começando no domingo
-    return "";
+    const date = new Date()
+    const day = date.getDay()
+    const daynames = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"];
+    return daynames[day]
 }
 
+updateContentHour();
 setInterval(updateContentHour, 1000);
 
 console.log(getCurrentTime());
 console.log(getCurrentDate());
+console.log(getWeekDay());
