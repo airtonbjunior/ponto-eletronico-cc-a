5ee3c73472c6a85117a5bba8e6ad79e13de8555e
airtonbjunior <airtonbjunior@gmail.com>
2024-11-18 10:51:36 -0300

    aula 18-11

--- js/criaUsuario.js
+++ js/criaUsuario.js
@@ -2,16 +2,17 @@ const btnCriaUsuario = document.getElementById("btn-cria-usuario");
 
 btnCriaUsuario.addEventListener('click', () => {
 
+
     fetch('http://localhost:3000/usuario', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(
             {
-            nome: "airton frontend",
-            email: "airtonfront@gmail.com",
-            senha: "oioioi",
-            login: "airtonairton",
-            permissao: "USER"
+            nome: document.getElementById("nome").value,
+            email: document.getElementById("email").value,
+            senha: document.getElementById("senha").value,
+            login: document.getElementById("login").value,
+            permissao: document.getElementById("permissao").value
             }
         )
     })
